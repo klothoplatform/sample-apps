84c4bf6fe71ec34a9f18ee338a240fdf72769047
Gordon Goetz <gordon.goetz@cloudcompiler.run>
2022-01-31 17:05:32 -0500

    Copy media sample app

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ This repo holds sample applications which demonstrate how to use Klotho to compi
 - [microservices](./ts-microservices) Cross execution unit function calls breaks the app into microservices while writing it like a monolith.
 - [serverless](./ts-serverless-gateway) Using `express` in a Klotho-compatible way to discover HTTP routes, automatically configuring gateway resources to route directly to the execution unit.
 - [graphql](./ts-graphql) How to use a framework in a Klotho-compatible way
+- [media storage](./ts-media-storage) Using a combination of blob storage for media (images) with key-value for metadata
 - [websocket](./ts-websocket-gateway) :construction: Set up a websocket gateway
---
+++ ts-media-storage/src/index.ts
@@ -0,0 +1,114 @@
+/**
+ * @keep_warm
+ * @compute_size 1core_512mb
+ * @topology_group imageapi
+ */
+
+import * as express from 'express';
+import * as fs from 'fs';
+var multer  = require('multer')
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '.png') //Appending .png
+  }
+})
+var upload = multer({ storage: storage })
+
+const { router, app } = setupExpressApp();
+
+/**
+ * @capability file_persist
+ */
+import cloudEnabledFS = require("fs/promises");
+
+/**
+* Persists the native Javascript Map when compiled
+* @capability kv_persist eventually_consistent
+*/
+let imageStore = new Map<string, string>([["options", {
+  mapId: "imageKV",
+  batchWrite: false,
+  writeOnChange: false
+} as any]]);
+
+imageStore.delete("options")
+
+function setupExpressApp() {
+  const app: any = express();
+  const router = express.Router();
+  let cors = require('cors');
+  router.use(cors());
+  router.use(express.urlencoded({ extended: true }));
+  router.use(express.json());
+  return { router, app };
+}
+
+function generateImagePath(id: string) {
+  if (process.env["CLOUDCC"] != "true") {
+    return `/tmp/${id}.png`
+  } else {
+    return `/images/${id}.png`
+  }
+}
+
+router.get('/', async (req, res) => {
+  res.send("Hello World!");
+});
+
+router.get('/v1/images/:id', async (req, res) => {
+  let path = await imageStore.get(req.params.id);
+  if (path) {
+    res.status(200).send(path)
+  } else {
+    res.status(404).send(`No image path found for id ${req.params.id}`)
+  }
+});
+
+router.post('/v1/images/:id', upload.single('image'), async (req, res) => {
+  try {
+    const imagePath = generateImagePath(req.params.id)
+    // use non-annotated fs to read image from /tmp/
+    const readStream = fs.createReadStream(req.file.path);
+    // use annotated fs to write file to cloud file store
+    const resultUrl = (await cloudEnabledFS.writeFile(imagePath, readStream, { flag: "w+" })) as any;
+    const url = resultUrl ? resultUrl : imagePath
+    // save the cloud stored object access url to annotated KV
+    await imageStore.set(req.params.id, url)
+    // remove the local copy of the image from /tmp/
+    fs.rmSync(req.file.path);
+    res.status(201).send(`Uploaded image to ${generateImagePath(req.params.id)}`)
+  } catch (err) {
+    res.status(500).send({
+      err: err,
+      msg: "Error Happened"
+    })
+  }
+});
+
+router.delete('/v1/images/:id', async (req, res) => {
+  // This isn't supported in cloud deployments yet. Will work locally though
+  let exists = await imageStore.has(req.params.id);
+  console.log(`Image exists: ${exists}`)
+  if (exists) {
+    let path = (await imageStore.get(req.params.id)) as string;
+    await cloudEnabledFS.rm(path)
+    res.status(204).end()
+  } else {
+    res.status(404).send(`No image found for id: ${req.params.id}`)
+  }
+})
+
+app.listen(3000, async () => {
+  console.log(`App listening locally`)
+})
+
+app.use(router)
+
+/**
+ * Connects the webapp to the Internet with an API Gateway
+ * @capability https_server
+ */
+exports.app = app
