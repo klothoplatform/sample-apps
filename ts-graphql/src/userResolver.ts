9b726fe5dd4254e666cedfbfaf171eee1badab37
ewucc <eric.wu@cloudcompiler.run>
2022-02-07 15:44:20 -0800

    minor adjustments

--- ts-graphql/src/userResolver.ts
+++ ts-graphql/src/userResolver.ts
@@ -1,29 +1,24 @@
 import { Query, Resolver, Mutation, Arg } from "type-graphql";
 import { User, UserInput } from "./user";
 
+const userStore = new Map<string, User>();
 
 @Resolver(of => User)
 export class UserResolver {
-  private userStore = new Map<string, User>();
-
-  @Query(returns => [User], { nullable: true })
-  async getUsers(): Promise<User[]> {
-    return await Array.from(this.userStore.values());
-  }
 
   @Query(returns => User, { nullable: true })
   async getUser(@Arg('userInput') { name }: UserInput): Promise<User> {
-    return await this.userStore.get(name);
+    return await userStore.get(name);
   }
 
   @Mutation(returns => User)
   async addUser(@Arg('userInput') { name }: UserInput): Promise<User> {
     const user = {
-      id: Math.floor(Math.random()*100),
+      id: Math.floor(Math.random() * 100),
       name,
     }
 
-    await this.userStore.set(user.name, user)
+    await userStore.set(user.name, user)
     return user;
   }
 }
