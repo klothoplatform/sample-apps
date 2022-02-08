b31884c482174288ca068dc283546f4bf5e8e7a4
ewucc <eric.wu@cloudcompiler.run>
2022-02-07 16:27:46 -0800

    add get all users call

--- ts-graphql/src/userResolver.ts
+++ ts-graphql/src/userResolver.ts
@@ -1,10 +1,22 @@
 import { Query, Resolver, Mutation, Arg } from "type-graphql";
 import { User, UserInput } from "./user";
 
-const userStore = new Map<string, User>();
+const userStore = new Map<string, User>([["options", {
+  mapId: "quoteKV",
+  batchWrite: false,
+  writeOnChange: false
+} as any]]);
+
+userStore.delete("options")
 
 @Resolver(of => User)
 export class UserResolver {
+
+  @Query(returns => [User], { nullable: true })
+  async getUsers(): Promise<User[]> {
+    let users = Array.from(await userStore.entries()).map(x => x[1])
+    return users
+  }
 
   @Query(returns => User, { nullable: true })
   async getUser(@Arg('userInput') { name }: UserInput): Promise<User> {
