5910e8633ecfe0e581b0d217b5690f84184f9123
Aaron Torres <atorres@klo.dev>
2022-02-28 12:23:54 -0600

    added a new sample app with scheduling, testing now

---
+++ ts-schedule/index.ts
@@ -0,0 +1,31 @@
+/**
+ * @topology_group main
+ */
+
+import fs = require("fs/promises");
+
+import * as express from 'express';
+const logger = require('lambda-log');
+
+const app = express()
+app.use(express.json())
+
+/**
+ * @capability schedule 0/5 * * * ? *
+ */
+export async function scheduledHealthSignal(){
+  // this method will be called every 5 minutes
+  logger.warn(`Health signal logged at ${Date.now()}`);
+}
+
+/**
+ * @capability https_server
+ */
+ app.listen(3000, async () => {
+  console.log(`App listening locally`)
+})
+
+export {app}
+
+
+
