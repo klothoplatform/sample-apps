434ddbfc233195bb9083f2e37d80a528a665c4f0
Aaron Torres <atorres@klo.dev>
2022-06-15 14:45:29 -0500

    pr feedback

--- ts-sequelize/model.ts
+++ ts-sequelize/model.ts
@@ -5,12 +5,21 @@
  */
 const { Sequelize, DataTypes } = require('sequelize');
 
+const sequelize = setupSequelize()
 
-// @klotho::persist
-const sequelize = new Sequelize(`sqlite::memory:`, {logging: false});
+async function setupSequelize() {
+  // @klotho::persist
+  const sequelize = new Sequelize(`sqlite::memory:`, {logging: false});
 
-const KV = sequelize.define('KV', {
-   
+  console.log("connecting")
+  await sequelize.authenticate()
+  await sequelize.sync({alter: true})
+  console.log("connected")
+  return sequelize
+}
+
+const KV = sequelize.then(async client =>  {
+  const KV = client.define('KV', {
     key: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -21,46 +30,36 @@ const KV = sequelize.define('KV', {
       allowNull: false
     }
   }, {
-   
-});
   
-const connect = (async () => {
-  console.log("starting up db connection!")
-  try {
-    await sequelize.authenticate().then;
-    await sequelize.sync({ force: true });
-    console.log('Connection has been established successfully and synced.');
+  })
+  await KV.sync({alter: true})
+  return KV;
+});
+
+
+export async function set(key:string, value:any){
+  try{
+    const client = await KV
+    const item = await client.upsert({key:key, value:value})
   } catch (error) {
-      console.error('Unable to connect to the database:', error);
+    console.error(`unable to set key:${key}, value:${value}. Received error:${error}`)
   }
-  // Code here
-})();
+}
 
-
-  export async function set(key:string, value:any){
-    try{
-      await connect
-      const item = await KV.upsert({key:key, value:value})
-    } catch (error) {
-      console.error(`unable to set key:${key}, value:${value}. Received error:${error}`)
+export async function get(key:string): Promise<any>{
+  try{
+    const client = await KV
+    const items = await client.findAll({
+        where:{
+            key: key
+        }
+    })
+    if (items.length == 1){
+        return items[0].value
     }
+  } catch (error) {
+    console.error(`unable to get key:${key}. Received error:${error}`)
   }
 
-  export async function get(key:string): Promise<any>{
-    try{
-      await connect
-      const items = await KV.findAll({
-          where:{
-              key: key
-          }
-      })
-
-      if (items.length == 1){
-          return items[0].value
-      }
-    } catch (error) {
-      console.error(`unable to get key:${key}. Received error:${error}`)
-    }
-
-    return undefined
-  }
+  return undefined
+}
