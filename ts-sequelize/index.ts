3df36f3bc462aee8d2133bf310a12d6f587bfeec
Aaron Torres <atorres@klo.dev>
2022-06-22 16:05:50 -0500

    updated sequelize to have types

--- ts-sequelize/index.ts
+++ ts-sequelize/index.ts
@@ -4,34 +4,30 @@
  * }
  */
 
-import * as express from 'express';
-import { set, get } from './model';
+import * as express from "express";
+import { set, get } from "./model";
 
+const app = express();
+app.use(express.json());
 
-const app = express()
-app.use(express.json())
+app.post("/item/", async (req, res) => {
+  const key = req.body["key"];
+  const value = req.body["value"];
 
-
-app.post('/item/', async (req, res) => {
-  var key = req.body["key"]
-  var value = req.body["value"]
-
-  await set(key, value)
-  res.send("success")
+  await set(key, value);
+  res.send("success");
 });
 
-
 app.get("/item/:key", async (req, res) => {
-  let value = await get(req.params["key"]);
+  const value = await get(req.params["key"]);
   res.send(value);
 });
 
-/* 
+/*
  * @klotho::expose {
  *  target = "public"
  * }
  */
 app.listen(3000, async () => {
-  console.log(`App listening locally`)
-})
-
+  console.log(`App listening locally`);
+});
