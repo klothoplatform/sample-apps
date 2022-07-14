31ba4cf592ace811b79e06971d3246e5b490e52c
gordon-klotho <gordon.goetz@klo.dev>
2022-07-14 13:09:38 -0400

    Move import to emitter.ts to be included in all exec units

--- ts-events/emitter.ts
+++ ts-events/emitter.ts
@@ -2,3 +2,6 @@ import events = require("events")
 
 // @klotho::pubsub
 export const MyEmitter = new events.EventEmitter();
+
+import "./hello_handler" // import after emitter set up to register handlers
+import "./other/handler"
