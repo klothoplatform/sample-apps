c27a9657442906cec10d88f6035bdf8c85d234cb
gordon-klotho <gordon.goetz@klo.dev>
2022-08-03 13:48:30 -0400

    Don't use `module.exports =` This stomps the other exports and could lead to problems down the road in other contexts, so don't do it in sample-apps which people may copy from

--- ts-serverless-gateway/src/users/post-users.ts
+++ ts-serverless-gateway/src/users/post-users.ts
@@ -8,12 +8,11 @@
  */
 
 
-const userPost = require('express').Router();
+export const router = require('express').Router();
 
 function postUsers(event, res) {
     let resp = `Hi ${event.body.user}`;
     res.send();
 }
 
-userPost.post('/v1/users', postUsers);
-module.exports = userPost;
+router.post('/v1/users', postUsers);
