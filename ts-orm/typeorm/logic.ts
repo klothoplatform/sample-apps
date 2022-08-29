c7e51a07a4545aa312385385b99cc2675c1e3a44
Jordan Singer <jordan.singer@klo.dev>
2022-08-29 07:45:00 -0500

    update ts-sequelize to ts-orm with typeorm

---
+++ ts-orm/typeorm/logic.ts
@@ -0,0 +1,44 @@
+import "reflect-metadata"
+import { DataSource, createConnection } from "typeorm"
+import { User } from "./model"
+
+
+export const initialize = async (): Promise<DataSource> => {
+
+   /** @klotho::persist  {
+   *   id = "typeormDB"
+   * }
+   */ 
+    const AppDataSource = new DataSource({
+        type: "postgres",
+        entities: [User],
+        synchronize: true,
+        logging: false,
+    })
+    
+    // to initialize initial connection with the database, register all entities
+    // and "synchronize" database schema, call "initialize()" method of a newly created database
+    // once in your application bootstrap
+    await AppDataSource.initialize()
+
+    return AppDataSource
+}
+
+const dataSource: Promise<DataSource> = initialize();
+
+
+export const write = async (firstName: string, lastName: string) => {
+    const user = new User()
+    user.firstName = firstName
+    user.lastName = lastName
+    await dataSource;
+    await User.save(user)
+}
+
+export const find = async (firstName: string) => {
+    await dataSource;
+    const response = await User.findOneBy({
+        firstName,
+    })
+    return response
+}
---
+++ ts-orm/typeorm/model.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm"
+
+@Entity()
+export class User extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    firstName: string
+
+    @Column()
+    lastName: string
+}
