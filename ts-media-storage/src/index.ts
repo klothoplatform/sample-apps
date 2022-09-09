7cb98460e5074b69704c2dcb212f071363fdda6a
David Septimus <david.septimus@klo.dev>
2022-09-09 15:43:39 -0400

    Updates ts-media-storage example - Updated annotations and removed references to old versions of klotho - Created separate endpoints for retrieving the content url and actual file content - Changed 'image' to 'media' - Removed '.png' file type limitation - Changed id to a filename rather an arbitrary identifier - Implemented delete for local files - Added Postman collection for ts-media-storage - Adds simple start and compile npm scripts

--- ts-media-storage/src/index.ts
+++ ts-media-storage/src/index.ts
@@ -1,27 +1,30 @@
 /**
  * @klotho::execution_unit {
- *   id = "media-imageapi"
- *   keep_warm = true
+ *   id = "media-file-api"
  * }
  */
 
 import * as express from 'express';
 import * as fs from 'fs';
-var multer  = require('multer')
-var storage = multer.diskStorage({
+import * as path from 'path';
+
+const port = 3000
+const multer = require('multer');
+const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/tmp/')
+    cb(null, '/tmp')
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '.png') //Appending .png
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
   }
-})
-var upload = multer({ storage: storage })
+});
+const upload = multer({ storage: storage });
 
 const { router, app } = setupExpressApp();
 
 /* @klotho::persist {
- * id = "cloudFs"
+ *   id = "cloudFs"
  * }
  */
 import cloudEnabledFS = require("fs/promises");
@@ -29,87 +32,108 @@ import cloudEnabledFS = require("fs/promises");
 /**
 * Persists the native Javascript Map when compiled
 * @klotho::persist {
-*  id = "imageKV"
+*   id = "mediaKV"
 * }
 */
-let imageStore = new Map<string, string>();
+const mediaStore = new Map<string, string>();
 
-imageStore.delete("options")
 
 function setupExpressApp() {
   const app: any = express();
   const router = express.Router();
-  let cors = require('cors');
+  const cors = require('cors');
   router.use(cors());
   router.use(express.urlencoded({ extended: true }));
   router.use(express.json());
   return { router, app };
 }
 
-function generateImagePath(id: string) {
-  if (process.env["CLOUDCC"] != "true") {
-    return `/tmp/${id}.png`
-  } else {
-    return `/images/${id}.png`
-  }
+function getStoragePath(id: string) {
+  return (process.env["CLOUDCC_NAMESPACE"]) ? `/media/${id}` : `/tmp/${id}`;
+}
+
+function getStoredFileName(filePath: string) {
+  const separator = process.env["CLOUDCC_NAMESPACE"] ? "/" : path.sep;
+  return filePath.split(separator).pop();
 }
 
 router.get('/', async (req, res) => {
   res.send("Hello from Klotho!");
 });
 
-router.get('/v1/images/:id', async (req, res) => {
-  let path = await imageStore.get(req.params.id);
+router.get('/v1/metadata/:id', async (req, res) => {
+  const path = await mediaStore.get(req.params.id);
   if (path) {
-    res.status(200).send(path)
+    res.status(200).send(path);
   } else {
-    res.status(404).send(`No image path found for id ${req.params.id}`)
+    res.status(404).send(`No media file path found for id ${req.params.id}`);
   }
 });
 
-router.post('/v1/images/:id', upload.single('image'), async (req, res) => {
+router.get('/v1/media/:id', async (req, res) => {
+  const id = req.params.id
+  const path = await mediaStore.get(id);
+  if (path) {
+    const fileName = getStoredFileName(id);
+    const file = await cloudEnabledFS.readFile(getStoragePath(id));
+    res.status(200)
+      .contentType(fileName)
+      .send(file);
+  }
+  else {
+    res.status(404).send(`No media file path found for id ${id}`);
+  }
+});
+
+router.post('/v1/media/:id',upload.single('media_file'), async (req, res) => {
+  console.log("start post")
   try {
-    const imagePath = generateImagePath(req.params.id)
-    // use non-annotated fs to read image from /tmp/
+    const storagePath = getStoragePath(req.params.id);
+    console.log(`path: ${storagePath}`)
+    // use non-annotated fs to read media file from /tmp/
     const readStream = fs.createReadStream(req.file.path);
     // use annotated fs to write file to cloud file store
-    const resultUrl = (await cloudEnabledFS.writeFile(imagePath, readStream, { flag: "w+" })) as any;
-    const url = resultUrl ? resultUrl : imagePath
+    const resultUrl = (await cloudEnabledFS.writeFile(storagePath, readStream, { flag: "w+" })) as any;
+    const url = resultUrl ? resultUrl : storagePath;
     // save the cloud stored object access url to annotated KV
-    await imageStore.set(req.params.id, url)
-    // remove the local copy of the image from /tmp/
+    console.log(`url: ${url}`)
+    await mediaStore.set(req.params.id, url);
+    // remove the local copy of the media file from /tmp/
     fs.rmSync(req.file.path);
-    res.status(201).send(`Uploaded image to ${generateImagePath(req.params.id)}`)
+    console.log("record updated")
+    res.status(201).send(`Uploaded media file to ${storagePath}`);
   } catch (err) {
+    console.log(err);
     res.status(500).send({
       err: err,
       msg: "Error Happened"
-    })
+    });
   }
 });
 
-router.delete('/v1/images/:id', async (req, res) => {
+router.delete('/v1/media/:id', async (req, res) => {
   // This isn't supported in cloud deployments yet. Will work locally though
-  let exists = await imageStore.has(req.params.id);
-  console.log(`Image exists: ${exists}`)
+  const exists = await mediaStore.has(req.params.id);
+  console.log(`Media file exists: ${exists}`);
   if (exists) {
-    let path = (await imageStore.get(req.params.id)) as string;
-    await cloudEnabledFS.rm(path)
-    res.status(204).end()
+    const path = (await mediaStore.get(req.params.id)) as string;
+    await cloudEnabledFS.rm(path);
+    await mediaStore.delete(path);
+    res.status(204).end();
   } else {
-    res.status(404).send(`No image found for id: ${req.params.id}`)
+    res.status(404).send(`No media file found for id: ${req.params.id}`);
   }
 })
 
 /* @klotho::expose {
- *  target = "public"
- *  id = "app"
+ *   target = "public"
+ *   id = "app"
  * }
  */
-app.listen(3000, async () => {
-  console.log(`App listening locally`)
+app.listen(port, async () => {
+  console.log(`App listening locally on port ${port}`);
 })
 
-app.use(router)
+app.use(router);
 
-exports.app = app
+exports.app = app;
