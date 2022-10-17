80df0cfd760e4d36af5a8412dd0e1cc7a33b1598
Jordan Singer <jordan.singer@klo.dev>
2022-10-17 16:24:50 -0500

    adding initial 3 integ tests

---
+++ ts-sequelize/test/happy_path.test.ts
@@ -0,0 +1,37 @@
+import {expect} from '@jest/globals';
+
+const testKey = "myTestKey"
+const testValue = "myTestValue"
+const axios = require('axios');
+const apiEndpoint = process.env['API_ENDPOINT']
+
+describe('Happy Path Test', () => {
+
+    beforeAll(() => {
+        // Eventually we will want to clear db, etc
+    });
+
+    test('Add User', async () => {
+        const actualPutResponse = await axios({
+            method: 'post',
+            url: `${apiEndpoint}/item/`,
+            data: {
+                key: testKey,
+                value: testValue
+              }
+        });
+        
+        const expectedPutResponse =  `success`
+        expect(actualPutResponse.data).toBe(expectedPutResponse)
+    })
+
+    test('Get User', async () => {
+        const actualGetResponse = await axios({
+            method: 'get',
+            url: `${apiEndpoint}/item/${testKey}`,
+        });
+        const users = actualGetResponse.data
+        expect(users).toBe(testValue)
+
+    })
+})
