8a4c8879675d2047c5013dc15bbd568a21836e93
Jordan Singer <jordan.singer@klo.dev>
2022-10-25 11:27:47 -0500

    ignore test dir

--- ts-embed_assets/test/pulumi/index.ts
+++ ts-embed_assets/test/pulumi/index.ts
@@ -1,6 +1,9 @@
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 import { PolicyPack, validateResourceOfType, StackValidationPolicy } from "@pulumi/policy";
+import {KlothoPolicyPackGenerator} from '../../../policy-pack-generator/policy-pack-generator'
+
+const gen = new KlothoPolicyPackGenerator('../../compiled/klotho.yaml')
 
 const lambdaPolicy: StackValidationPolicy = {
     name: "lambda-test",
@@ -17,11 +20,11 @@ const lambdaPolicy: StackValidationPolicy = {
           const lambda = lambdaObj.asType(aws.lambda.Function)!
           if (lambda.memorySize !== 512) {
               reportViolation(
-                  `Expected rds Instance '${lambda.name}' allocated storage to be '512' but found '${lambda.memorySize}'`);
+                  `Expected lambda function '${lambda.name}' memory size to be '512' but found '${lambda.memorySize}'`);
           }
           if (lambda.timeout !== 180) {
               reportViolation(
-                  `Expected rds Instance '${lambda.name}' allocated storage to be '180' but found '${lambda.timeout}'`);
+                  `Expected lambda function '${lambda.name}' timeout to be '180' but found '${lambda.timeout}'`);
           }
         }
         // TODO: check remainder of the resources set by klotho.yaml or others we want to validate
