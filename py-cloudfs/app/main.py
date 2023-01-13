1b21593e0e25c59262e6f36580fab8f29d12d06a
ewucc <eric.wu@cloudcompiler.run>
2023-01-13 12:57:05 -0800

    adding py test for cloudfs

---
+++ py-cloudfs/app/main.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from fastapi import FastAPI
+from datetime import datetime
+# @klotho::persist {
+#   id = "fs"
+# }
+import aiofiles
+
+helloPath = "/tmp/hello.txt"
+
+# @klotho::expose {
+#   id = "cloudfs-gw"
+#   target = "public"
+# }
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+  await setup()
+
+
+  async with aiofiles.open(helloPath, mode='r') as f:
+    result = await f.read()
+  
+    return result
+
+
+async def setup():
+  t = datetime.now()  
+  async with aiofiles.open(helloPath, mode='w') as f:
+    await f.write("Startup at {}".format(t))
+
