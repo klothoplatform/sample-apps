5704f54b5514e822de6086fcfcf90067b5dc3683
gordon-klotho <gordon.goetz@klo.dev>
2023-02-02 17:21:42 -0500

    Initial commit of sample C# web api

---
+++ cs-simple/Models/Person.cs
@@ -0,0 +1,13 @@
+namespace SampleApp.Models
+{
+  public class Person
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public override string ToString()
+    {
+      return $"{FirstName} {LastName}";
+    }
+  }
+}
