0f396f168f89cd827fb82b24103b9f5d5abb853f
Jordan Singer <jordan.singer@klo.dev>
2023-03-06 15:22:54 -0500

    make clustername valid

--- _test_resources/index.ts
+++ _test_resources/index.ts
@@ -191,7 +191,7 @@ const proxyTarget = new aws.rds.ProxyTarget(`${dbName}`, {
 
 // elasticache redis resources
 
-const clusterName = "elasticacheRedis"
+const clusterName = "elasticache-redis"
 const logGroupName = `/aws/elasticache/${clusterName}-persist-redis`
 let cloudwatchGroup = new aws.cloudwatch.LogGroup(`persist-redis-${clusterName}-lg`, {
     name: `${logGroupName}`,
@@ -232,7 +232,7 @@ new aws.elasticache.Cluster(
 
 
 // memorydb resources
-const memdbClusterName = 'memorydbCluster'
+const memdbClusterName = 'memorydb-cluster'
 const memdbSubnetGroup = new aws.memorydb.SubnetGroup(`${memdbClusterName}-subnetgroup`,{
         subnetIds: vpc.privateSubnetIds,
         tags: {
